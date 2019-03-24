330e65af262f1480982c2c6bb33124f16fc21584
IreneKandidatova <47784868+IreneKandidatova@users.noreply.github.com>
2019-03-24 17:43:38 +0300

    add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+var zeroNumber = number;
+  for (i=2; i<=base; i++){
+    if (base % i ==0){
+      var countFactorials = 0;
+      var countDivide = 0;
+         while (base % i == 0){
+         countFactorials++;
+        base = base / i;
+         }
+       var tempNumber = number;
+         while (tempNumber / i >0){
+           countDivide+= Math.floor(tempNumber/i);    
+           tempNumber = Math.floor(tempNumber / i);  
+      }
+    var zeroNumber = Math.min(zeroNumber, countDivide/countFactorials );
+    }  
+  }
+  zeroNumber = Math.floor(zeroNumber);
+  return zeroNumber}
